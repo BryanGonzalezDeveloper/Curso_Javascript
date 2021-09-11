2dd6dab6711231f8785dd4e2d4030b714ac8c966
BryanGonzalezDeveloper <gonzalezzbryan220@gmail.com>
2021-09-11 07:55:48 -0700

    Operadores con javascript

---
+++ Operadores/script.js
@@ -0,0 +1,23 @@
+//Operadores matematicos
+console.log("Operadores matematicos");
+let a=5;
+let b=7;
+console.log(a+b);
+console.log(a*b);
+console.log(b-a);
+console.log(b%a);
+console.log("Programación"+" "+"con"+" JS.");
+//Operadores de asignacion
+console.log("Operadores de asignación.");
+let numero=10;
+console.log("valor original:" +numero);
+numero+=5;
+console.log(numero);
+numero-=5;
+console.log(numero);
+numero*=2;
+console.log(numero);
+numero/=2;
+console.log(numero);
+numero%=3;
+console.log(numero);
