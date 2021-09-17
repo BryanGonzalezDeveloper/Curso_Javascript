f506ff86d40f30f5da28104d2a5e85f5290c2e37
BryanGonzalezDeveloper <gonzalezzbryan220@gmail.com>
2021-09-17 10:06:51 -0700

    Arrays declaracion basica

---
+++ arrays/SintaxisBasica/script.js
@@ -0,0 +1,6 @@
+//let numeros=[];Declaracion array vacio
+let numeros=[1,3,5,7,9];//Declaracion array inicializado.
+console.log(numeros);
+
+console.log('Acceder a una posición especifica del array');
+console.log(numeros[4]);
