561962b909632e700914d874c004d0e7dfa05711
BryanGonzalezDeveloper <gonzalezzbryan220@gmail.com>
2021-09-20 18:50:01 -0700

    peticion utilizando Ajax

---
+++ peticiones_http/script.js
@@ -0,0 +1,10 @@
+//Solicitud web utilizando AJAX:
+
+const Http = new XMLHttpRequest();
+const url='https://jsonplaceholder.typicode.com/posts';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
